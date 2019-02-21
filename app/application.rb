ef30f14e5fed00108ddb3bb0ad245db6b639586b
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-21 02:38:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+
+    if Time.now >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
   end
 
 end
